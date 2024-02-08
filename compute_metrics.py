6f076f8dea21e23c9fcce9b253f001a7b25eea2b
Justin Chiu <chiu.justin.t@gmail.com>
2024-02-08 01:27:03 -0500

    compute metrics within a single file only

--- compute_metrics.py
+++ compute_metrics.py
@@ -8,10 +8,11 @@ for setting in [
     'llemma7b_minif2f_test',
     #'llemma34b_minif2f_test'
 ]:
-    fs = [json.load(open(x)) for x in glob.glob('./output/%s/*/*.json' % setting)]
-    n = 0
-    ns = 0
-    for f in fs:
+    fs = [x for x in glob.glob('./output/%s/*/*.json' % setting)]
+    for x in fs:
+        f = json.load(open(x))
+        n = 0
+        ns = 0
         for result in f['results']:
             name = result['example']['full_name']
 
@@ -22,5 +23,5 @@ for setting in [
             n += 1
             if result['success']:
                 ns += 1
-    if n > 0:
-        print(setting, ns/n, ns, n, sep='\t')
+        if n > 0:
+            print(setting, x, ns/n, ns, n, sep='\t')
