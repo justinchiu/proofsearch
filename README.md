df96ad90f2238b8e01d8699d3bd5fc7133a9d58c
Justin Chiu <chiu.justin.t@gmail.com>
2024-02-08 01:33:24 -0500

    update readme and change scripts back to validation

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Relies on
 * [Llemma](https://github.com/EleutherAI/math-lm)
 * [VLLM](https://github.com/vllm-project/vllm)
 
+# Baseline results
+Run `bash scripts/eval_llemma7b.sh` to run Llemma 7b (with awq quantization)
+on the minif2f validation data.
+Compute the accuracy with `python compute_metrics.py`.
+This should get around 26% accuracy.
--- scripts/eval_llemma7b.sh
+++ scripts/eval_llemma7b.sh
@@ -6,15 +6,15 @@ NUM_SAMPLES=32
 TEMPERATURES="0.0"
 TIMEOUT=600
 NUM_SHARDS=1
-#DATASET="minif2f-valid"
-DATASET="minif2f-test"
+DATASET="minif2f-valid"
+#DATASET="minif2f-test"
 DATA="data/minif2f.jsonl"
 
 MODEL="open-web-math/llemma_7b"
 NAME="llemma7b"
 
-#OUTPUT_DIR="output/${NAME}_minif2f_valid"
-OUTPUT_DIR="output/${NAME}_minif2f_test"
+OUTPUT_DIR="output/${NAME}_minif2f_valid"
+#OUTPUT_DIR="output/${NAME}_minif2f_test"
 
 #for SHARD in 0 1 2 3
 for SHARD in 0
